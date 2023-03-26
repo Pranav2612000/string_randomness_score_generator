fefda9b9f4209f21fb79e3195d7b5136a9f69130
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-26 20:41:45 +0530

    feat: modify index.js to make it a package

--- modelConsumer/index.js
+++ modelConsumer/index.js
@@ -1,20 +1,3 @@
 const Model = require('./Model');
-const main = () => {
-    console.log('Running');
-    Model.loadModel();
 
-    const inputs = [
-        "HelloWorldThisIsALongStringAlthoughItShoudln'tbeclassifedasecretalthoithsspelngmistakes",
-        "AKIAYVP4CIPPERUVIFXG",
-        "authorizationToken",
-        "12345",
-        "qwertyuiop",
-        "qazwsxedcrfvtgbyhn",
-        "key-Axkljh408834j7",
-        "Key-^jkfah^_(_()_)(*)"
-    ];
-    const outputs = inputs.map(x => ({ word: x, score: Model.score(x)}));
-    console.log(outputs);
-}
-
-main();
+module.exports = Model;
