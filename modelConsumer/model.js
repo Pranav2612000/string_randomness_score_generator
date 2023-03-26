cae8ea6bcb15b95179ad48282478d77f3da197c7
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-26 17:04:36 +0530

    improv: store charMap into memory

--- modelConsumer/model.js
+++ modelConsumer/model.js
@@ -2,14 +2,21 @@ const defaultModelFile = require('../data/model.json');
 
 class Model {
     probabilities = [];
+    charMap = {};
     constructor() {
         this.probabilities = [];
+        this.charMap = {}
     }
 
     loadModel(modelFile = defaultModelFile) {
         console.log("Loading model...");
+
+        let i = 0;
         for (const ch of modelFile.chars) {
+            this.charMap[ch] = i;
             this.probabilities.push(modelFile.data[ch]);
+
+            i++;
         }
     }
 }
